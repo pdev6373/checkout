7e10cda02b3d5c3d30da3f01ef8287ae5f44dac3
pdev6373 <pdev6373@gmail.com>
2023-06-09 15:01:54 +0100

    added api

--- app/[productId]/page.tsx
+++ app/[productId]/page.tsx
@@ -39,7 +39,7 @@ export default function Checkout({ params }: CheckoutType) {
     setLoading(true);
 
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/transactions/checkout`,
+      `${process.env.NEXT_PUBLIC_PAYMENT_BASE_URL}/transactions/checkout`,
       {
         body: JSON.stringify(values),
         headers: {
